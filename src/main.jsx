066cdac4b685e9def4387cb8162b5fdf25f8b079
hafarna03aja-droid <hafarna03aja@gmail.com>
2025-11-18 16:13:46 +0700

    Wrap app with ApiProvider for global API key/provider context

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AuthProvider } from './contexts/AuthContext'
+import { ApiProvider } from './contexts/ApiProviderContext';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <ApiProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </ApiProvider>
   </React.StrictMode>,
 )
